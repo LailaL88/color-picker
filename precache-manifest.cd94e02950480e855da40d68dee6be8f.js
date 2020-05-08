ee6658f227f1a270a38db7aa59801424dff0933c
Olga <olga_ze@inbox.lv>
2020-05-08 11:07:00 +0300

    Updates

---
+++ precache-manifest.cd94e02950480e855da40d68dee6be8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4151008af7b2f93ee487815f2f0534",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "6e82e88f008da9039735",
+    "url": "/color-picker/static/css/main.1f18fef5.chunk.css"
+  },
+  {
+    "revision": "26cd5ee7a101d143a28d",
+    "url": "/color-picker/static/js/2.d934012f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-picker/static/js/2.d934012f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e82e88f008da9039735",
+    "url": "/color-picker/static/js/main.e2c98799.chunk.js"
+  },
+  {
+    "revision": "58d38afce6430f0a1851",
+    "url": "/color-picker/static/js/runtime-main.c55dfbfc.js"
+  }
+]);
